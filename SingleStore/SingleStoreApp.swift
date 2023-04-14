54006eb663bed1baab33f8dd7627b67daa335d58
fatbobman <xuyang@me.com>
2023-04-14 16:15:51 +0800

    Initial Commit

---
+++ SingleStore/SingleStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleStoreApp.swift
+//  SingleStore
+//
+//  Created by Yang Xu on 2023/4/14.
+//
+
+import SwiftUI
+
+@main
+struct SingleStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
